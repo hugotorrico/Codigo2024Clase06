2ca788c3fac5967dbbf5371030aba6834c5da13e
Hugo Torrico <torricomarquezh@gmail.com>
2024-09-13 20:26:12 -0500

    Ejemplo funciones listas

--- Codigo2024Clase06/Program.cs
+++ Codigo2024Clase06/Program.cs
@@ -46,35 +46,140 @@ void EjemploListInt()
     }
 }
 
-
-//input
-List<int> edades = new List<int>();
-edades.Add(20);
-edades.Add(30);
-edades.Add(21);
-edades.Add(18);
-
-//edades.Sort();
-//edades.Remove(30);
-//Console.WriteLine( edades.Contains(30));
-
-
-////business logic
-//foreach (var item in edades)
-//{
-//    if (item%2==0)
-//    {
-//        edades.Remove(item);
-//    }
-//}
-
-//output
-
-foreach (var item in edades)
+void EjemploFuncionesAddSSortRemove()
 {
-    Console.WriteLine(item);
+    //input
+    List<int> edades = new List<int>();
+    edades.Add(20);
+    edades.Add(30);
+    edades.Add(21);
+    edades.Add(18);
+
+    //edades.Sort();
+    //edades.Remove(30);
+    //Console.WriteLine( edades.Contains(30));
+
+    //output
+
+    foreach (var item in edades)
+    {
+        Console.WriteLine(item);
+    }
+
 }
 
+
+int CalcularMenorNotasListas(List<int> notas)
+{
+    int menor = notas[0];
+
+    foreach (var item in notas)
+    {
+        if (item<menor)
+        {
+            menor = item;
+
+        }
+
+    }
+    return menor;
+}
+
+int CalcularMayorNotasListas(List<int> notas)
+{
+    int mayor = notas[0];
+
+    foreach (var item in notas)
+    {
+        if (item > mayor)
+        {
+            mayor = item;
+
+        }
+
+    }
+    return mayor;
+}
+
+void ejemploFuncionesMinMax()
+{
+    List<int> notas = new List<int>();
+
+    notas.Add(20);
+    notas.Add(15);
+    notas.Add(12);
+    notas.Add(18);
+
+
+    Console.WriteLine(notas.Min());//Usando funciones de la misma lista
+    Console.WriteLine(CalcularMenorNotasListas(notas));
+
+    Console.WriteLine(notas.Max());//Usando funciones de la misma lista
+    Console.WriteLine(CalcularMayorNotasListas(notas));
+}
+
+void ejemploExpresionesLambda()
+{
+    List<int> notas = new List<int>();
+
+    notas.Add(20);
+    notas.Add(15);
+    notas.Add(12);
+    notas.Add(18);
+
+    //Expresiones Lambda
+    var notaspares = notas.Where(x => x % 2 == 0).ToList();
+
+    foreach (var item in notaspares)
+    {
+        Console.WriteLine(item);
+    }
+
+}
+
+
+void ejemploFuncionIndexOf()
+{
+    List<int> numeros = new List<int>();
+
+    Console.WriteLine("Ingresa 7 números:");
+
+    for (int i = 0; i < 3; i++)
+    {
+        Console.Write($"Número {i + 1}: ");
+        int numero = int.Parse(Console.ReadLine());
+        numeros.Add(numero);
+    }
+
+    int numeroBuscar = 10;
+    int indice = numeros.IndexOf(numeroBuscar);
+
+    //Cuando índice vale -1, no lo encontró
+    if (indice != -1)
+        Console.WriteLine($"El número {numeroBuscar} se encuentra en el índice {indice}.");
+    else
+        Console.WriteLine($"El número {numeroBuscar} no está en la lista.");
+}
+
+void ejemploAddRange()
+{
+    List<string> paisesAmerica = new List<string>();
+    paisesAmerica.Add("Perú");
+    paisesAmerica.Add("Bolivia");
+    paisesAmerica.Add("Colombia");
+    paisesAmerica.Add("Chile");
+    paisesAmerica.Add("Ecuador");
+
+    List<string> paisesEuropa = new List<string>();
+    paisesEuropa.Add("Francia");
+    paisesEuropa.Add("Inglaterra");
+    paisesEuropa.Add("Alemania");
+    paisesEuropa.Add("España");
+
+    List<string> paises = new List<string>();
+    paises.AddRange(paisesAmerica);
+    paises.AddRange(paisesEuropa);
+}
 
 
 

92b3006077f9a6acc982fd8f7b3d77ad47b1a1f8
Hugo Torrico <torricomarquezh@gmail.com>
2024-09-13 19:29:32 -0500

    Agregar archivos de proyecto.

---
+++ Codigo2024Clase06/Program.cs
@@ -0,0 +1,81 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+void EjemploListString()
+{
+   
+    List<string> nombres = new List<string>();
+
+    nombres.Add("Hugo");
+    nombres.Add("Juan");
+    nombres.Add("Janeth");
+
+
+    for (int i = 0; i < nombres.Count; i++)
+    {
+        Console.WriteLine(nombres[i]);
+    }
+
+    Console.WriteLine("=================");
+
+    //Usando el tipo de dato dinámico var
+    foreach (var item in nombres)
+    {
+        Console.WriteLine(item);
+    }
+
+    //Usando el tipo de dato original
+    foreach (string nombre in nombres)
+    {
+        Console.WriteLine(nombre);
+    }
+
+}
+
+void EjemploListInt()
+{
+    List<int> edades = new List<int>();
+
+    edades.Add(20);
+    edades.Add(30);
+    edades.Add(21);
+    edades.Add(18);
+
+    foreach (var item in edades)
+    {
+        Console.WriteLine(item);
+    }
+}
+
+
+//input
+List<int> edades = new List<int>();
+edades.Add(20);
+edades.Add(30);
+edades.Add(21);
+edades.Add(18);
+
+//edades.Sort();
+//edades.Remove(30);
+
+
+
+////business logic
+//foreach (var item in edades)
+//{
+//    if (item%2==0)
+//    {
+//        edades.Remove(item);
+//    }
+//}
+
+//output
+
+foreach (var item in edades)
+{
+    Console.WriteLine(item);
+}
+
+
+
+
+Console.Read();
